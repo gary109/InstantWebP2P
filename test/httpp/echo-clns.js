29f8518c4ab7b9b610eed41343e144fb5d99c0cd
tom zhou <zs68j2ee@gmail.com>
2012-11-20 17:47:01 +0800

    fixed httpp test

--- test/httpp/echo-clns.js
+++ test/httpp/echo-clns.js
@@ -1,13 +1,15 @@
 
 var udt = require('udt');
-var cln = udt.connect({port:51686, host: 'localhost'} , function(){
+//var cln = udt.connect({port:51686, host: 'localhost'} , function(){
+var cln = udt.connect({port:51686, host: '192.168.1.106'} , function(){
     console.log('you can type char here, then server send it back:\n');
     process.stdin.resume();
     process.stdin.pipe(cln);   
     cln.pipe(process.stdout); 
 });
 
-var cln1 = udt.connect({port:51868, host: 'localhost'} , function(){
+//var cln1 = udt.connect({port:51868, host: 'localhost'} , function(){
+var cln1 = udt.connect({port:51868, host: '192.168.1.106'} , function(){
     console.log('you can type char here, then server send it back:\n');
     process.stdin.resume();
     process.stdin.pipe(cln);   
--- test/httpp/httpp-clns.js
+++ test/httpp/httpp-clns.js
@@ -1,17 +1,33 @@
 var httpp = require('httpp');
+var http = require('http');
+var maxSrvs = 2;
+var maxClns = 10;
 
-httpp.get('http://localhost:51688', function(res){
-   console.log('STATUS: ' + res.statusCode);
-   console.log('HEADERS: ' + JSON.stringify(res.headers));
-   res.on('data', function (chunk) {
-   console.log('BODY: ' + chunk);
-  });
-});
 
-httpp.get('http://localhost:51866', function(res){
-   console.log('STATUS: ' + res.statusCode);
-   console.log('HEADERS: ' + JSON.stringify(res.headers));
-   res.on('data', function (chunk) {
-   console.log('BODY: ' + chunk);
-  });
-});
+// concurrent clients
+for (var i = 0; i < maxSrvs; i ++) {
+  var port = 51688+i;
+  
+  for (var j = 0; j < maxClns; j ++) {
+    // httpp clients
+    //httpp.get('http://localhost:'+port, function(res){
+    httpp.get('http://192.168.1.105:'+port, function(res){
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+    });
+  
+    // http clients
+    //http.get('http://localhost:'+port, function(res){
+    httpp.get('http://192.168.1.105:'+port, function(res){
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+    });
+  }
+}
+
