7864ffc6fee2d706857c8743f3457f9c6076fd2e
tom zhou <zs68j2ee@gmail.com>
2013-06-16 23:22:35 +0800

    fixed pSetFileCompletionNotificationModes on udt.c

--- deps/uv/src/win/udt.c
+++ deps/uv/src/win/udt.c
@@ -82,6 +82,8 @@ static int uv_udt_set_socket(uv_loop_t* loop, uv_udt_t* handle,
     if (pSetFileCompletionNotificationModes((HANDLE) socket,
         FILE_SKIP_SET_EVENT_ON_HANDLE |
         FILE_SKIP_COMPLETION_PORT_ON_SUCCESS)) {
+      handle->flags |= UV_HANDLE_SYNC_BYPASS_IOCP;
+    } else if (GetLastError() != ERROR_INVALID_FUNCTION) {
       uv__set_sys_error(loop, GetLastError());
       return -1;
     }
