bb58aebbe5d1699f98771784833c38dc331547e1
tom zhou <zs68j2ee@gmail.com>
2013-09-30 15:39:48 +0800

    Update stream.c

--- deps/uv/src/unix/stream.c
+++ deps/uv/src/unix/stream.c
@@ -523,9 +523,9 @@ start:
     while (n == -1 && errno == EINTR);
   } else {
 	  if (stream->type == UV_UDT) {
-		  int next = 1;
+		  int next = 1, it = 0;
 		  n = -1;
-		  for (int it = 0; it < iovcnt; it ++) {
+		  for (it = 0; it < iovcnt; it ++) {
 			  size_t ilen = 0;
 			  while (ilen < iov[it].iov_len) {
 				  int rc = udt_send(((uv_udt_t *)stream)->udtfd, ((char *)iov[it].iov_base)+ilen, iov[it].iov_len-ilen, 0);
