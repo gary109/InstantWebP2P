434485573bae2a5d99304f6bc34c8683e14fd527
tom zhou <zs68j2ee@gmail.com>
2013-11-23 14:47:15 +0800

    fixed httpp test typo

--- test/httpp/http-srv.js
+++ test/httpp/http-srv.js
@@ -3,4 +3,4 @@ var srv = http.createServer(function(req, res){
   res.end('Hi, just say hi to you over TCP ...\n');
 });
 srv.listen(51680);
-console.log('HTTPP server listing on TCP port 51688');
+console.log('HTTPP server listing on TCP port 51680');
