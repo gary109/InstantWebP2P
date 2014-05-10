75eb6138110187f52916c196f399e503cd082276
tom zhou <zs68j2ee@gmail.com>
2014-05-10 10:29:21 +0800

    code cleanup on udt,udts,httpp,httpps module

--- cross-compile-pi.sh
+++ cross-compile-pi.sh
@@ -1,11 +1,11 @@
 ### change toolchain to yours
-export AR=arm-linux-gnueabihf-ar
-export CC=arm-linux-gnueabihf-gcc
-export CXX=arm-linux-gnueabihf-g++
-export LINK=arm-linux-gnueabihf-g++
+export AR=arm-bcm2708hardfp-linux-gnueabi-ar
+export CC=arm-bcm2708hardfp-linux-gnueabi-gcc
+export CXX=arm-bcm2708hardfp-linux-gnueabi-g++
+export LINK=arm-bcm2708hardfp-linux-gnueabi-g++
 
 ### add options, like --prefix=/opt/node-v0.8.x-pi/
 ./configure --without-snapshot --dest-cpu=arm --dest-os=linux --prefix=/opt/node-v0.8.x-pi/
 
-make
+make && make install
  
