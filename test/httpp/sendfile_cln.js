955ddf089fa590621168da753a3f6ccfaf348d6a
tom zhou <zs68j2ee@gmail.com>
2014-07-29 10:00:45 +0800

    fixed httpp sendfile_srv/cln.js with CLI arguments

--- test/httpp/sendfile_cln.js
+++ test/httpp/sendfile_cln.js
@@ -1,14 +1,42 @@
 var udt = require('udt');
 var fs = require('fs');
 
-var cln = udt.connect({port:51666, host: 'localhost'}, function(){
-    console.log('UDP file client connected to localhost:51666');
+// check arguments
+if (process.argv.length < 4) {
+	console.log('Usage: node sendfile_clnt.js host-or-ip port\n');
+	process.exit(-1);
+}
+var host = process.argv[2];
+var port = parseInt(process.argv[3], 10);
+
+var cln = udt.connect({port: port, host: host}, function(){
+    console.log('UDP file client connected to '+host+':'+port);
     var file_stream = fs.createReadStream('./dummy/file_send.txt');
     
     file_stream.pipe(cln);
     
+    // network performance monitor
+    var npm = setInterval(function(){
+
+    	var perf = cln.netPerf;
+
+    	console.log('socket network Bandwidth       :'+JSON.stringify(perf.mbpsBandwidth)+' Mb/s');
+    	console.log('socket network RTT             :'+JSON.stringify(perf.msRTT)+' ms');
+    	console.log('socket network PktSndPeriod    :'+JSON.stringify(perf.usPktSndPeriod)+' us');
+    	console.log('socket network SendRate        :'+JSON.stringify(perf.mbpsSendRate)+' Mb/s');
+    	console.log('socket network RecvRate        :'+JSON.stringify(perf.mbpsRecvRate)+' Mb/s');
+    	console.log('socket network CongestionWindow:'+JSON.stringify(perf.pktCongestionWindow));
+    	console.log('socket network RecvACK         :'+JSON.stringify(perf.pktRecvACK));
+    	console.log('socket network RecvNACK        :'+JSON.stringify(perf.pktRecvNAK));
+    	console.log('socket network AvailRcvBuf     :'+JSON.stringify(perf.byteAvailRcvBuf));
+    	console.log('socket network AvailSndBuf     :'+JSON.stringify(perf.byteAvailSndBuf)+'\n\n');
+
+    }, 6000); // 6s
+    
+    // closure
     cln.on('end', function(){
         console.log('send file done');
+        clearInterval(npm);
     });
 });
 
--- test/httpp/sendfile_srv.js
+++ test/httpp/sendfile_srv.js
@@ -1,6 +1,13 @@
 var udt = require('udt');
 var fs = require('fs');
 
+// check arguments
+if (process.argv.length < 3) {
+	console.log('Usage: node sendfile_srv.js port\n');
+	process.exit(-1);
+}
+var port = parseInt(process.argv[2], 10);
+
 var srv = udt.createServer(function(socket){
     var fn = './dummy/file_recv@'+Date.now()+'.txt';
     var file_stream = fs.createWriteStream(fn);
@@ -8,5 +15,5 @@ var srv = udt.createServer(function(socket){
     console.log('write file to '+fn);
 });
 
-srv.listen(51666);
-console.log('UDP file server listen on 51666');
+srv.listen(port);
+console.log('UDP file server listen on '+port);
